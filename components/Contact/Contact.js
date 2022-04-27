c0e4e5c8c56a8c3b3cea42f629ff81be84b98cb5
serkancagman <serkancagmann@gmail.com>
2022-04-27 14:48:17 +0300

    Finish contact page

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -2,6 +2,9 @@ import React from "react";
 import style from "./style/Contact.module.css";
 import { useFormik } from "formik";
 import { validationSchema } from "./Validation";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 const Contact = () => {
   const { values, handleChange, handleBlur, touched, errors, handleSubmit } =
     useFormik({
@@ -48,7 +51,9 @@ const Contact = () => {
                       name="name"
                       onBlur={handleBlur}
                     />
-                    {errors.name && touched.name && ( <span className={style.errorMessage}>{errors.name}</span> )}
+                    {errors.name && touched.name && (
+                      <span className={style.errorMessage}>{errors.name}</span>
+                    )}
                   </div>
                   <div className="col-lg-6 col-md-6 col-12">
                     <label className={style.formLabel} htmlFor="email">
@@ -63,7 +68,9 @@ const Contact = () => {
                       onChange={handleChange}
                       onBlur={handleBlur}
                     />
-                    {errors.email && touched.email && ( <span className={style.errorMessage}>{errors.email}</span> )}
+                    {errors.email && touched.email && (
+                      <span className={style.errorMessage}>{errors.email}</span>
+                    )}
                   </div>
                 </div>
                 <div className="row g-3">
@@ -80,7 +87,9 @@ const Contact = () => {
                       id="phone"
                       onBlur={handleBlur}
                     />
-                    {errors.phone && touched.phone && ( <span className={style.errorMessage}>{errors.phone}</span> )}
+                    {errors.phone && touched.phone && (
+                      <span className={style.errorMessage}>{errors.phone}</span>
+                    )}
                   </div>
                   <div className="col-lg-6 col-md-6 col-12">
                     <label className={style.formLabel} htmlFor="subject">
@@ -95,7 +104,11 @@ const Contact = () => {
                       onChange={handleChange}
                       onBlur={handleBlur}
                     />
-                    {errors.subject && touched.subject && ( <span className={style.errorMessage}>{errors.subject}</span> )}
+                    {errors.subject && touched.subject && (
+                      <span className={style.errorMessage}>
+                        {errors.subject}
+                      </span>
+                    )}
                   </div>
                 </div>
                 <div className="row g-3">
@@ -113,7 +126,11 @@ const Contact = () => {
                       rows="6"
                       onChange={handleChange}
                     ></textarea>
-                    {errors.message && touched.message && ( <span className={style.errorMessage}>{errors.message}</span> )}
+                    {errors.message && touched.message && (
+                      <span className={style.errorMessage}>
+                        {errors.message}
+                      </span>
+                    )}
                   </div>
                 </div>
                 <button
@@ -124,6 +141,37 @@ const Contact = () => {
                   Send Message
                 </button>
               </form>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-12">
+            <div className={style.contactInfo}>
+              <div className={style.contactInfoHeader}>
+                <h2 className={style.contactInfoTitle}> Contact Info</h2>
+                <p className={style.contactInfoSubTitle}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolore dolor quam repudiandae.
+                </p>
+              </div>
+              <div className="d-flex flex-column justify-content-center align-items-start">
+                <div className="d-flex justify-content-start my-2 align-items-center">
+                  <HiOutlineLocationMarker className={style.contactInfoIcon} />
+                  <p className={style.contactInfoText}>
+                    48 Brooke Street, New York United States of America
+                  </p>
+                </div>
+                <div className="d-flex justify-content-start my-2 align-items-center">
+                  <BsFillTelephoneFill className={style.contactInfoIcon} />
+                  <p className={style.contactInfoText}>+1 (234) 567 8901</p>
+                </div>
+                <div className="d-flex justify-content-start my-2 align-items-center">
+                  <FiMail className={style.contactInfoIcon} />
+                  
+                    <a className={style.contactInfoText} href="mailto:cagmanserkann@gmail.com">
+                      cagmanserkann@gmail.com
+                    </a>
+                
+                </div>
+              </div>
             </div>
           </div>
         </div>
